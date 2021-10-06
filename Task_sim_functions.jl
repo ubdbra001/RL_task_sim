7f37bb3e436f0b2d7d0ebd6b71dd2cd814740ed9
ubdbra001 <dan.t.brady21@gmail.com>
2021-10-06 19:25:56 +0100

    Specified input vector type and updated comments

--- Task_sim_functions.jl
+++ Task_sim_functions.jl
@@ -1,13 +1,13 @@
 using ElasticArrays, Plots
 
-function softmax(V::Vector; B = 1)
-# Function for calculating softmaxed values
+function softmax(V::AbstractVector{<:Real}; B = 1)
+# Converts Vector of values into softmaxed values
+# (Sum to one)
 # Inputs:
-#   V: Vector of values 
+#   V: Vector of real values 
 #   B: Temp parameter (default 1)
 # Output:
 #   V_softmax: Softmaxed values of V
     V_softmax = exp.(B * V)/sum(exp.(B * V))
 	return(V_softmax)
 end
-
