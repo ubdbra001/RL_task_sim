e2b62d0f092bb42a2e28a16b0a9795e40f45296b
ubdbra001 <dan.t.brady21@gmail.com>
2021-10-06 19:11:12 +0100

    added softmax function

---
+++ Task_sim_functions.jl
@@ -0,0 +1,13 @@
+using ElasticArrays, Plots
+
+function softmax(V::Vector; B = 1)
+# Function for calculating softmaxed values
+# Inputs:
+#   V: Vector of values 
+#   B: Temp parameter (default 1)
+# Output:
+#   V_softmax: Softmaxed values of V
+    V_softmax = exp.(B * V)/sum(exp.(B * V))
+	return(V_softmax)
+end
+
