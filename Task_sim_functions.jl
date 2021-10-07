669b03645a8f0e63c494ad73d5cb2f18e9ab370b
ubdbra001 <dan.t.brady21@gmail.com>
2021-10-07 09:15:12 +0100

    Added method for single value argumet for softmax

--- Task_sim_functions.jl
+++ Task_sim_functions.jl
@@ -5,9 +5,22 @@ function softmax(V::AbstractVector{<:Real}; B = 1)
 # (Sum to one)
 # Inputs:
 #   V: Vector of real values 
-#   B: Temp parameter (default 1)
+#   B: Temp parameter (default = 1)
 # Output:
-#   V_softmax: Softmaxed values of V
+#   V_softmax: Vector with softmaxed values of V
     V_softmax = exp.(B * V)/sum(exp.(B * V))
 	return(V_softmax)
+end
+
+function softmax(R_in::Real; B = 1)
+# Dispatch to deal with single number being sent to function
+# instead of vector
+# Inputs:
+#   R_in: Real number
+#   B: Temp parameter (default = 1)
+# Output:
+#   V_softmax: Vector with softmaxed values of V
+
+    V_softmax = softmax([R_in]; B)
+    return(V_softmax)
 end
