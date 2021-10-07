32dff03577013c009201ba2ac3df9636659c5b70
ubdbra001 <dan.t.brady21@gmail.com>
2021-10-07 20:31:32 +0100

    Added RW_learning function

--- Task_sim_functions.jl
+++ Task_sim_functions.jl
@@ -56,3 +56,58 @@ function select_response(V::AbstractVector{<:Real})::Int
 end
 
 
+
+function RW_learning(values, responses, α_vals,
+    correct_resp::Int, cr_switch::Int, max_trials;;Int, fuzziness::Float)
+# Recursive reversal learning task takes in properties and produces a matrix of
+# values for each of the stimuli
+# Will call itself until the specified max number of trials are reached
+# Inputs:
+#   values: Matrix containing present and past values for the stimuli
+#   responses: Vector of integers representing responses given by the agent
+#   α_vals: Dict containg positive and negative reward values
+#   correct_resp: Integer indicating the location of the current correct response
+#   cr_switch: Integer specifying number of trials before the correct response changes
+#   max_trials: Integer specifying the number of trials before exiting
+#   fuzziness: Float specifying the probability that the correct response will be rewarded
+# Outputs:
+#   values: Matrix of the values for each stimuli
+
+    # Get the most recent values for the stimuli and softmax them
+	V_curr = values[:,end]
+	V_soft = softmax(V_curr)
+	
+    # Select a response and add that to the response vector
+	Q = select_response(V_soft)
+	push!(responses, Q)
+
+    # Check whether that response is rewarded or not
+	Q_vec = check_answer(correct_resp, Q, length(V_curr), fuzziness)
+	
+    # Calculate the delta
+	δ = (Q_vec - V_curr)
+
+    # Adjust the reward values according to the delta
+	α = dualLearningRate(α_vals, δ)
+
+    # Generate the stimuli values for the current trial and add that to the
+    # values matrix  
+	V_new = V_curr .+ (α .* δ)
+	append!(values, V_new)
+	
+    # How many trials have been completed
+	N_trials = length(responses)
+	
+    # If we've reached a switch point then choose a new correct response
+	if N_trials % cr_switch == 0
+		correct_resp = new_correct(length(V_curr); prev_corr = correct_resp)
+	end
+	
+    # If we've reached the max number of trials then break out of the function
+	if N_trials == max_trials
+		return(values)
+	else # Otherwise run the function again with the new values
+		RW_learning(values, responses, α_vals, correct_resp, cr_switch, max_trials, fuzziness)
+	end
+	
+end
